a89d6390e5a88629bfbf92d049fa94e0709ff20c
gabrielcabralbfr <gabrielcabralbfr@gmail.com>
2018-07-26 04:10:03 -0300

    [add] getting all users from collection (needs fixing)

--- node-mongo-intro/routes/index.js
+++ node-mongo-intro/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const mongodb = require('mongodb');
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hell' });
+  // res.sendFile(path.join(__dirname,'..', 'views', 'index.html'));
+  res.render('index', {title: "HELL"})
 });
 
 module.exports = router;
--- node-mongo-intro/routes/users.js
+++ node-mongo-intro/routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const mongodb = require('mongodb');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index', {title: 'PAGINA DE USUARIO'});
 });
+
+router.get('/listar', function(req, res) {
+  var MongoClient = mongodb.MongoClient;
+  var url = 'mongodb://localhost:27017/user_db';
+
+  MongoClient.connect(url, { useNewUrlParser: true}, function(err, client) {
+    const collection = client.db('user_db').collection('users');
+
+    collection.find({}).toArray(function(err, result) {
+      if (err) {
+        res.send(err);
+      } else if (result.length) {
+        console.log(result);
+        res.render('userlist', {userlist : result})
+      } else {
+        res.send("Nothing found here!")
+      }
+
+      client.close();
+    });
+  })
+})
+
+
 
 module.exports = router;
